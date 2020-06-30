2fd54b849d4f5b97e89e7ba6cc6e022d704d0940
Igor de Andrade Tudsico <igortudisco@hotmail.com>
2020-06-30 16:17:29 -0300

    Criando a Classe PosicaoXadrez

--- GameChess/GameChess/Program.cs
+++ GameChess/GameChess/Program.cs
@@ -10,13 +10,10 @@ namespace GameChess
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-
-                Tela.imprimirtabuleiro(tab);
+                Console.WriteLine(pos);
+                Console.WriteLine(pos.toPosicao());
 
                 Console.ReadLine();
             }
---
+++ GameChess/GameChess/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,34 @@
+﻿using tabuleiro;
+
+namespace Xadrez
+{
+
+    // classe para começar a colocar os nomes de cada posição do jogo de xadrez
+
+    class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        // metodo de converção
+        // As letras corespondem a um numero sendo a = 0, b = 1 e assim por diante.
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+
+
+    }
+}
