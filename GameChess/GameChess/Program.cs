a9ad2ab93ab19908b0cefbe69b9bda1a5a052a09
Igor de Andrade Tudsico <igortudisco@hotmail.com>
2020-06-30 15:29:45 -0300

    Tratando erros com exceção personalizada

--- GameChess/GameChess/Program.cs
+++ GameChess/GameChess/Program.cs
@@ -8,15 +8,23 @@ namespace GameChess
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            Tela.imprimirtabuleiro(tab);
+                Tela.imprimirtabuleiro(tab);
 
-            Console.ReadLine();
+                Console.ReadLine();
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+
 
         }
     }
--- GameChess/GameChess/Tabuleiro/Tabuleiro.cs
+++ GameChess/GameChess/Tabuleiro/Tabuleiro.cs
@@ -1,4 +1,6 @@
 ﻿
+using tabuleiro;
+
 namespace tabuleiro
 {
     class Tabuleiro
@@ -22,13 +24,54 @@ namespace tabuleiro
             return pecas[linha, coluna];
         }
 
+        // melhoria de posição
+
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        // Existe peça no tabuleiro 
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         // Colocando uma peca no tabuleiro
 
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
+
+        // verificando o tamanho do tabuleiro
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // exeção
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida ");
+            }
+        }
+
 
     }
 }
