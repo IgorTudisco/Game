32f5d5df45ba8c96aff7c6f085abc5d5e8eb4d49
Igor de Andrade Tudsico <igortudisco@hotmail.com>
2020-06-30 16:48:00 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- GameChess/GameChess/Program.cs
+++ GameChess/GameChess/Program.cs
@@ -10,10 +10,15 @@ namespace GameChess
         {
             try
             {
-                PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-                Console.WriteLine(pos);
-                Console.WriteLine(pos.toPosicao());
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(2, 3));
+
+                Tela.imprimirtabuleiro(tab);
 
                 Console.ReadLine();
             }
